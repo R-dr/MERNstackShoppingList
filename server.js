ca87c11ff8e0e73f3d0c9b157fd65c60af3544f4
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-02-15 15:15:57 +1100

    post merge commit

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const items = require('./routes/api/items')
+const items = require("./routes/api/items");
 
-
-
-const app = express()
+const app = express();
 
 // Bodyparser middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //Db Config
-const db = require('./config/keys').mongoURI
+const db = require("./config/keys").mongoURI;
 
 //connect to mongo
 
-mongoose.connect(db)
-.then(()=>console.log("MongoDB connected"))
-.catch(e => console.log(e))
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch((e) => console.log(e));
 
 // Use routes
-app.use('/api/items',items)
+app.use("/api/items", items);
 
-const port = process.env.PORT || 5000
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-app.listen(port,() => console.log(`Server started on port ${port}`))
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
