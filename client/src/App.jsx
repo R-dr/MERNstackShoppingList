748f8833de053366ba94101ca157acc4887ca14a
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-02-15 13:46:32 +1100

    Redux store complete moving on the the database connections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import AppNavBar from './components/AppNavBar'
-import ShoppingList from './components/ShoppingList'
+import React from "react";
+import AppNavBar from "./components/AppNavBar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import {Container} from 'reactstrap'
 
-import {Provider} from 'react-redux'
-import store from './store'
+import { Provider } from "react-redux";
+import store from "./store";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 function App() {
   return (
     <Provider store={store}>
-
       <div className="App">
-      <AppNavBar/>
-      <ShoppingList/>
+        <AppNavBar />
+        <Container>
+
+          <ItemModal/>
+         <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
